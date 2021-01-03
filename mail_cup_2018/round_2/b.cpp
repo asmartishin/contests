d1a63f5721b1225bb7dc3c7a90d7bdff82185a25
Aleksei Martishin <alekseim@amazon.com>
2021-01-03 22:55:59 +0100

    update DSU

--- mail_cup_2018/round_2/b.cpp
+++ mail_cup_2018/round_2/b.cpp
@@ -17,6 +17,62 @@ using namespace std;
 
 #define endl '\n'
 
+class DSU {
+public:
+    size_t setsCount = 0;
+
+    DSU(const size_t size)
+        : parent(size, -1)
+        , rank(size, -1)
+    {}
+
+    bool make_set(int element) {
+        if (parent[element] > -1) {
+            return false;
+        }
+        ++setsCount;
+        parent[element] = element;
+        rank[element] = 0;
+        return true;
+    }
+
+    int find_set(int x) {
+        if (x != parent[x]) {
+            parent[x] = find_set(parent[x]);
+        }
+        return parent[x];
+    }
+
+    bool union_sets(int x, int y) {
+        int xp = find_set(x);
+        int yp = find_set(y);
+
+        if (xp == yp) {
+            return false;
+        }
+        --setsCount;
+
+        if (rank[xp] < rank[yp]) {
+            parent[xp] = yp;
+        } else if (rank[xp] > rank[yp]) {
+            parent[yp] = xp;
+        } else {
+            parent[yp] = xp;
+            ++rank[xp];
+        }
+
+        return true;
+    }
+
+    void print() {
+        for (size_t i = 0; i < parent.size(); ++i) {
+            cout << i << ' ' << parent[i] << endl;
+        }
+    }
+private:
+    vector<int> parent;
+    vector<int> rank;
+};
 
 class TUnionSet {
 public:
@@ -83,7 +139,7 @@ int main() {
     cin >> n >> m >> l;
 
     vector<long long> hairs(n);
-    TUnionSet unionSet(n);
+    DSU unionSet(n);
 
     for (size_t i = 0; i < n; ++i) {
         cin >> hairs[i];
@@ -91,10 +147,10 @@ int main() {
 
     for (size_t i = 0; i < n; ++i) {
         if (hairs[i] > l) {
-            unionSet.MakeSet(i);
+            unionSet.make_set(i);
 
             if (i > 0 && hairs[i - 1] > l) {
-                unionSet.Union(i - 1, i);
+                unionSet.union_sets(i - 1, i);
             }
         }
     }
@@ -103,20 +159,20 @@ int main() {
         cin >> t;
 
         if (t == 0) {
-            cout << unionSet.SetsCount << endl;
+            cout << unionSet.setsCount << endl;
         } else {
             cin >> p >> d;
             --p;
             hairs[p] += d;
 
             if (hairs[p] > l) {
-                unionSet.MakeSet(p);
+                unionSet.make_set(p);
                 if (p > 0 && hairs[p - 1] > l) {
-                    unionSet.Union(p - 1, p);
+                    unionSet.union_sets(p - 1, p);
                 }
 
                 if (p < n - 1 && hairs[p + 1] > l) {
-                    unionSet.Union(p, p + 1);
+                    unionSet.union_sets(p, p + 1);
                 }
             }
         }
